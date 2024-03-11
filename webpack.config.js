24424f92bf84bddd8c955ea775b42eaa125ec8ed
div11R <divyanshudw64@gmail.com>
2024-03-11 20:33:53 +0530

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,5 @@ module.exports = {
     output:{
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        static: {
-          directory: path.resolve(__dirname, 'public'), // Serve static files from the 'public' directory
-        },
-        compress: true, // Enable gzip compression
-        port: 9000, // Port to serve the application on (default is 8080)
-        open: true, // Automatically open the application in the browser
-      }
-
+    }
 }
